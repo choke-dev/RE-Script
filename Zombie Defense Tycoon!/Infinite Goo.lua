8bd9fed2da7acbb4fe7f336430b2f941616c63a3
choke <47597850+choke-dev@users.noreply.github.com>
2021-03-11 09:17:37 +0800

    Create Infinite Goo.lua

---
+++ Zombie Defense Tycoon!/Infinite Goo.lua
@@ -0,0 +1,9 @@
+local args = {
+   [1] = "PlaceObject",
+   [2] = "Turrets",
+   [3] = "Cyber Turret",
+   [4] = CFrame.new(Vector3.new(-43.819507598877, 2.2761368751526, 300.11749267578), Vector3.new(-1, -4.3711388286738e-08, -0)),
+   [5] = -8008135
+}
+
+game:GetService("ReplicatedStorage").CIient:FireServer(unpack(args))
