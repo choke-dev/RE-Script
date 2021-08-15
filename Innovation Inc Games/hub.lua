956f08ece024c7a716f744193d863c20ab32b29f
choke <47597850+choke-dev@users.noreply.github.com>
2021-08-15 18:17:25 +0800

    Create hub.lua

---
+++ Innovation Inc Games/hub.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 331811267 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/rscripts/main/Innovation%20Inc%20Games/IIS.lua?token=ALLESGSQ3BT7LXU2IVWYFOLBEIR32"))()
+end
+if game.PlaceId == 1033860623 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/rscripts/main/Innovation%20Inc%20Games/IAB.lua?token=ALLESGW3QNOG6CAMRRD67J3BEIR72"))()
+end
