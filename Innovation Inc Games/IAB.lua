694d78a90b8adeb5833f5ba204209c980f6bf77e
choke <47597850+choke-dev@users.noreply.github.com>
2021-08-16 12:44:28 +0800

    Update IAB.lua

--- Innovation Inc Games/IAB.lua
+++ Innovation Inc Games/IAB.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterCompat.lua"))() -- make this shit compatible with other exploits
+--loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterCompat.lua"))() -- make this shit compatible with other exploits
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/rscripts/main/Universal/functions?token=ALLESGWJZN7IILSX4RQE2T3BEMDYK"))() -- the functions i probably dont need lol
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() --Initialize the UI Lib
 local MainWindow = Library.CreateLib("Artic Base Remote Control Panel by choke#3588", "Ocean")
