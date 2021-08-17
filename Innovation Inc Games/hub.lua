f91a8ce512a97264cff06436297db081efadfd8e
choke-dev <sfranzdaniel@gmail.com>
2021-08-17 16:07:04 +0800

    i forgot a () lol

--- Innovation Inc Games/hub.lua
+++ Innovation Inc Games/hub.lua
@@ -4,3 +4,6 @@ end
 if game.PlaceId == 1033860623 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Innovation%20Inc%20Games/IAB.lua?token=ALLESGQOXJQ7ITZ5DCMEAVLBERV6O"))()
 end
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Innovation%20Inc%20Games/hub.lua?token=ALLESGWTKLHCCZWWX23FEKLBESUB2"))()
+--use the commented loadstring to use this script
