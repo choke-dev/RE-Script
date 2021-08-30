734bd12e4872991ab31af130de27fa151950675b
choke <47597850+choke-dev@users.noreply.github.com>
2021-08-30 17:25:58 +0800

    Update Clue ESP.lua

--- MURDER/Clue ESP.lua
+++ MURDER/Clue ESP.lua
@@ -7,7 +7,7 @@ while wait() do
                     if not prop:FindFirstChild("BoxHandleAdornment") then
                       local clues = Instance.new("BoxHandleAdornment")
                       local BillboardGui = Instance.new("BillboardGui")
-				              local TextLabel = Instance.new("TextLabel")
+				      local TextLabel = Instance.new("TextLabel")
                         clues.Transparency = 0.3
                         clues.Color = BrickColor.new(1020)
                         clues.Parent = prop
@@ -15,22 +15,22 @@ while wait() do
                         clues.ZIndex = 0
                         clues.Adornee = prop.Parent
                         clues.AlwaysOnTop = true
-				                BillboardGui.Adornee = prop.Parent
-				                BillboardGui.Name = "Loot"
-				                BillboardGui.Parent = prop
-				                BillboardGui.Size = UDim2.new(0, 100, 0, 150)
-				                BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
-				                BillboardGui.AlwaysOnTop = true
-                        TextLabel.Parent = BillboardGui
-				                TextLabel.BackgroundTransparency = 1
-				                TextLabel.Position = UDim2.new(0, 0, 0, -50)
-				                TextLabel.Size = UDim2.new(0, 100, 0, 100)
-				                TextLabel.Font = Enum.Font.SourceSansSemibold
-				                TextLabel.TextSize = 20
-			    	            TextLabel.TextColor3 = Color3.new(1, 1, 1)
-			    	            TextLabel.TextStrokeTransparency = 0
-			    	            TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
-			    	            TextLabel.Text = 'Loot'
+				        BillboardGui.Adornee = prop.Parent
+				        BillboardGui.Name = "Loot"
+				        BillboardGui.Parent = prop
+				        BillboardGui.Size = UDim2.new(0, 100, 0, 150)
+				        BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
+				        BillboardGui.AlwaysOnTop = true
+				        TextLabel.Parent = BillboardGui
+				        TextLabel.BackgroundTransparency = 1
+				        TextLabel.Position = UDim2.new(0, 0, 0, -50)
+				        TextLabel.Size = UDim2.new(0, 100, 0, 100)
+				        TextLabel.Font = Enum.Font.SourceSansSemibold
+				        TextLabel.TextSize = 20
+			    	    TextLabel.TextColor3 = Color3.new(1, 1, 1)
+			    	    TextLabel.TextStrokeTransparency = 0
+			    	    TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+			    	    TextLabel.Text = 'Loot'
                   end
               end
           end
