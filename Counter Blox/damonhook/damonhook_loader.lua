437e0b3b187c3b184f89107b29b58f2838411d15
choke <47597850+choke-dev@users.noreply.github.com>
2022-03-27 16:23:18 +0800

    Create damonhook_loader.lua

---
+++ Counter Blox/damonhook/damonhook_loader.lua
@@ -0,0 +1,3 @@
+getgenv().Keybind = Enum.KeyCode.F1
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Counter%20Blox/damonhook/damonhook_source.lua?token=GHSAT0AAAAAABNSALBD2CXKWINCNQMQU5ZQYSJLEGQ"))()
