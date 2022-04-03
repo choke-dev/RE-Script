83a364c308c09bf961c6bea3bf58bccc1381310d
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-03 12:51:57 +0800

    Update RoleList.lua

--- Bloxston Mystery/RoleList.lua
+++ Bloxston Mystery/RoleList.lua
@@ -1,29 +1,25 @@
+---@diagnostic disable: undefined-global
 local Players = game:GetService("Players")
-local HTTP = game:GetService("HttpService")
+local Teams = game:GetService("Teams")
+
 if game.PlaceId ~= 8326774084 then return end
-repeat wait() until game:IsLoaded()
-repeat wait() until Players.LocalPlayer:WaitForChild("PlayerData").Number.Value ~= 0
 if not getgenv().Loaded then
-    task.wait(4)
+    repeat task.wait() until game:IsLoaded()
+    repeat task.wait() until #Teams.Dead:GetPlayers() ~= 0 -- waits for all players to load in
+    repeat task.wait() until #Teams.Dead:GetPlayers() == 0 -- waits for the game to start
 end
 getgenv().Loaded = true
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/UI%20Libs/Mercury.lua?token=GHSAT0AAAAAABNSALBCJMGK5FCI32E2CG5SYSL45DA"))()
-
-local FakeNames = {}
-
-for i,v in pairs(Players:GetChildren()) do
-    local int = v.PlayerData.Number.Value
-    local str = v.PlayerData.DisplayName.Value
-    local role = v.PlayerData.Role.Value
-    table.insert(FakeNames, "["..int.."] "..str.." | "..v.Name.." | "..role)
-end
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/UI%20Libs/Mercury.lua"))()
 
 local gui = Library:create{
-    Theme = Library.Themes.Serika,
+    Name = "BM GUI",
+    Theme = Library.Themes.Rust,
     Link = "https://chokes.software/bloxston-mystery",
 }
-gui:set_status("[IDLE] Waiting for events...")
+gui:set_status("GUI Version: 1.1 | Stable")
+
+-- // Role List Tab
 
 local tab = gui:tab{
     Icon = "rbxassetid://6034767607",
@@ -32,10 +28,26 @@ local tab = gui:tab{
 
 local Dropdown = tab:dropdown({
     Name = "Click me to view the role list.",
+    Description = "A red text means that player is dead / communicating with the dead.",
     StartingText = "Open...",
-    Items = FakeNames
+    Items = {}
 })
-FakeNames = {} -- table no longer used, clearing it to save memory
+
+for i,v in pairs(Players:GetChildren()) do
+    local int = v.PlayerData.Number.Value
+    local str = v.PlayerData.DisplayName.Value
+    local role = v.PlayerData.Role.Value
+    if tostring(v.Team) == "Dead" then
+        Dropdown:AddItems({
+            "<b><font color=\"rgb(196, 77, 77)\">["..int.."] "..str.." | "..v.Name.." | "..role.."</font></b>"
+        })
+    else
+        Dropdown:AddItems({
+            "["..int.."] "..str.." | "..v.Name.." | "..role
+        })
+    end
+    task.wait()
+end
 
 local RefreshButton = tab:button({
     Name = "Refresh role list",
@@ -46,28 +58,49 @@ local RefreshButton = tab:button({
             local int = v.PlayerData.Number.Value
             local str = v.PlayerData.DisplayName.Value
             local role = v.PlayerData.Role.Value
-            Dropdown:AddItems({
-                "["..int.."] "..str.." | "..v.Name.." | "..role
-            })
+            if tostring(v.Team) == "Dead" then
+                Dropdown:AddItems({
+                    "<b><font color=\"rgb(196, 77, 77)\">["..int.."] "..str.." | "..v.Name.." | "..role.."</font></b>"
+                })
+            else
+                Dropdown:AddItems({
+                    "["..int.."] "..str.." | "..v.Name.." | "..role
+                })
+        end
             task.wait() -- necessary or the dropdown won't update and it'll just return an empty dropdown lol
         end
         gui:Notification{
             Title = "Success",
             Text = "Role list refreshed.",
-            Duration = 2.5,
+            Duration = 5,
             Callback = function() end
         }
     end
 })
+
+-- // Role List Tab
+
+gui:Credit{
+	Name = "choke",
+	Description = "Script creator",
+	V3rm = "https://v3rmillion.net/member.php?action=profile&uid=1773404",
+	Discord = "choke#3588"
+}
 
 Players.PlayerRemoving:Connect(function(player)
     local int = player.PlayerData.Number.Value
     local str = player.PlayerData.DisplayName.Value
     local role = player.PlayerData.Role.Value
     gui:set_status("[PLAYER LEAVE] Updating role list...")
-    Dropdown:RemoveItems({
-            "["..int.."] "..str.." | "..player.Name.." | "..role	-- just the names to get removed (upper/lower case ignored)
-    })
+    if tostring(player.Team) == "Dead" then
+        Dropdown:RemoveItems({
+            "<b><font color=\"rgb(196, 77, 77)\">["..int.."] "..str.." | "..player.Name.." | "..role.."</font></b>"
+        })
+    else
+        Dropdown:RemoveItems({
+            "["..int.."] "..str.." | "..player.Name.." | "..role
+        })
+    end
     gui:set_status("[SUCCESS] Role list updated. | ["..int.."] "..str.." | "..role.." has been removed.")
 end)
 
