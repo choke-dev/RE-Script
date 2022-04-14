9bb37931626b85a1408f85ff5f3d8c8398386ff9
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-14 17:21:18 +0800

    Update Hint ESP.lua

--- Outlaster!/Hint ESP.lua
+++ Outlaster!/Hint ESP.lua
@@ -1,9 +1,18 @@
---if game.PlaceId ~= 5343994291 or game.PlaceId ~= 5765666934 then return end
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+if game.PlaceId ~= 5765666934 then 
+    Notify({
+        Title = "<b><font color=\"rgb(255, 0, 0)\">❌  Error!</font></b>";
+        Description = "You must join an Outlaster game to use this script!";
+        Duration = 15;
+    });
+    return 
+end
 
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
-local CS = game:GetService("CollectionService")
-local foundhints = 0
+local CoSe = game:GetService("CollectionService")
+local ContextActionService = game:GetService("ContextActionService")
+local contextButton = false
 
 local function WTS(part)
     local screen = workspace.CurrentCamera:WorldToViewportPoint(part.Position)
@@ -42,7 +51,7 @@ local function ESP(part, text, color)
             if destroyed and name ~= nil then
                 name:Remove()
                 box:Remove()
-                CS:RemoveTag(part, "ESP")
+                CoSe:RemoveTag(part, "ESP")
                 RdSt:Disconnect()
             end
             if part ~= nil then
@@ -61,36 +70,64 @@ local function ESP(part, text, color)
     end)
 end
 
+local function search()
+if contextButton then
+    contextButton = false
+    return
+else
+    contextButton = true
+end
+
+local foundhints = 0
+
 for i,v in pairs(workspace:GetDescendants()) do
-    if v:IsA("TouchTransmitter") and v.Parent:IsA("MeshPart") and v.Parent.Parent:IsA("Model") and v.Parent.Parent.Parent:IsA("MeshPart") and v.Parent.Parent.Parent.Parent:IsA("Model") and not CS:HasTag(v, "ESP") then
-        foundhints = foundhints + 1
+    if v:IsA("TouchTransmitter") and v.Parent:IsA("MeshPart") and v.Parent.Parent:IsA("Model") and v.Parent.Parent.Parent:IsA("MeshPart") and v.Parent.Parent.Parent.Parent:IsA("Model") and not CoSe:HasTag(v, "ESP") then
         ESP(v.Parent, "Hint", Color3.fromRGB(0,255,0))
-        CS:AddTag(v, "ESP")
-        Notify({
-            Title = "<b><font color=\"rgb(255, 0, 0)\">Wait...</font></b>";
-            Description = "Teleporting hint...";
-            Duration = 1;
-        });
-        --v.Parent.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+        CoSe:AddTag(v, "ESP")
+        foundhints = foundhints + 1
     end
 end
 
 if foundhints ~= 0 then
     Notify({
-        Title = "<b><font color=\"rgb(0, 255, 0)\">Success!</font></b>";
+        Title = "<b><font color=\"rgb(0, 255, 0)\">✓  Success!</font></b>";
         Description = "Found <b><font color=\"rgb(97, 255, 126)\">"..foundhints.." hints</font></b>!";
         Duration = 5;
     });
-elseif #game:GetService("Players"):GetPlayers() <= 5 then
+else
     Notify({
-        Title = "<b><font color=\"rgb(255, 0, 0)\">Error!</font></b>";
-        Description = "Game needs 5+ players to spawn new <b><font color=\"rgb(97, 255, 126)\">hints</font></b>!\nCurrently, there are "..#game:GetService("Players"):GetPlayers().." players.";
+        Title = "<b><font color=\"rgb(255, 255, 0)\">⚠️  Warning</font></b>";
+        Description = "No new <b><font color=\"rgb(97, 255, 126)\">hints</font></b> were found!";
+        Duration = 2.5;
+    });
+end
+end
+
+ContextActionService:BindAction("V | Search", search, false, Enum.KeyCode.V)
+Notify({
+    Title = "<b><font color=\"rgb(56, 137, 196)\">ℹ  Notification</font></b>";
+    Description = "Press <b><font color=\"rgb(255, 255, 0)\">[ V ]</font></b> to search for <b><font color=\"rgb(97, 255, 126)\">hints</font></b>!";
+    Duration = 15;
+});
+
+--[[
+    if foundhints ~= 0 then
+    Notify({
+        Title = "<b><font color=\"rgb(0, 255, 0)\">✓  Success!</font></b>";
+        Description = "Found <b><font color=\"rgb(97, 255, 126)\">"..foundhints.." hints</font></b>!\n\nCurrently, there are "..#game:GetService("Players"):GetPlayers().." players.";
+        Duration = 5;
+    });
+elseif #game:GetService("Players"):GetPlayers() < 5 then
+    Notify({
+        Title = "<b><font color=\"rgb(255, 0, 0)\">❌  Error!</font></b>";
+        Description = "Game needs 6+ players to spawn\nnew <b><font color=\"rgb(97, 255, 126)\">hints</font></b>!\n\nCurrently, there are "..#game:GetService("Players"):GetPlayers().." players.";
         Duration = 5;
     });
 elseif foundhints == 0 then
     Notify({
-        Title = "<b><font color=\"rgb(255, 255, 0)\">Warning</font></b>";
-        Description = "No <b><font color=\"rgb(97, 255, 126)\">hints</font></b> were found!";
+        Title = "<b><font color=\"rgb(255, 255, 0)\">⚠️  Warning</font></b>";
+        Description = "No new <b><font color=\"rgb(97, 255, 126)\">hints</font></b> were found!\n\nCurrently, there are "..#game:GetService("Players"):GetPlayers().." players.";
         Duration = 5;
     });
 end
+]]
