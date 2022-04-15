9d579eb8c72ed0b9606795c1ce8155e8c742bc8e
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-15 09:49:35 +0800

    ya

---
+++ Dependencies/Drawing Library 3D Box ESP.lua
@@ -0,0 +1,148 @@
+--// Made by Blissful#4992
+
+-- // Editied a bit by choke#3588 to remove the esp when the part is destroyed
+
+--// Locals:
+local workspace = game:GetService("Workspace")
+local player = game:GetService("Players").LocalPlayer
+local camera = workspace.CurrentCamera
+
+local module = {}
+
+--// Settings:
+local on = true -- Use this if your making gui
+
+local Box_Thickness = 2
+local Box_Transparency = 1 -- 1 Visible, 0 Not Visible
+
+local Autothickness = true -- Makes screen less encumbered
+
+local function NewLine(color)
+    local Box_Color = color or Color3.fromRGB(255, 0, 0)
+    local line = Drawing.new("Line")
+    line.Visible = false
+    line.From = Vector2.new(0, 0)
+    line.To = Vector2.new(1, 1)
+    line.Color = Box_Color
+    line.Thickness = Box_Thickness
+    line.Transparency = Box_Transparency
+    return line
+end
+
+--// Main Function:
+--[[
+    Example: 
+    local Part = workspace.Part      
+    ESP(Part)
+]]
+function module.ESP(objected, color)
+    local part = objected
+
+    --// Lines for 3D box (12)
+    local lines = {
+        line1  = NewLine(color),
+        line2  = NewLine(color),
+        line3  = NewLine(color),
+        line4  = NewLine(color),
+        line5  = NewLine(color),
+        line6  = NewLine(color),
+        line7  = NewLine(color),
+        line8  = NewLine(color),
+        line9  = NewLine(color),
+        line10 = NewLine(color),
+        line11 = NewLine(color),
+        line12 = NewLine(color)
+    }
+
+    --// Updates ESP (lines) in render loop
+    local function Updater()
+        local connection
+        connection = game:GetService("RunService").RenderStepped:Connect(function()
+            pcall(function()
+                local partpos, onscreen = camera:WorldToViewportPoint(part.Position)
+                local destroyed = not objected:IsDescendantOf(workspace)
+                if destroyed then
+                    for i,v in pairs(lines) do
+                        v:Remove()
+                    end
+                    connection:Disconnect()
+                end
+                if on and onscreen then
+                    local size_X = part.Size.X/2
+                    local size_Y = part.Size.Y/2
+                    local size_Z = part.Size.Z/2
+
+                    local Top1 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(-size_X, size_Y, -size_Z)).p)
+                    local Top2 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(-size_X, size_Y, size_Z)).p)
+                    local Top3 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(size_X, size_Y, size_Z)).p)
+                    local Top4 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(size_X, size_Y, -size_Z)).p)
+
+                    local Bottom1 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(-size_X, -size_Y, -size_Z)).p)
+                    local Bottom2 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(-size_X, -size_Y, size_Z)).p)
+                    local Bottom3 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(size_X, -size_Y, size_Z)).p)
+                    local Bottom4 = camera:WorldToViewportPoint((part.CFrame * CFrame.new(size_X, -size_Y, -size_Z)).p)
+
+                    --// Top:
+                    lines.line1.From = Vector2.new(Top1.X, Top1.Y)
+                    lines.line1.To = Vector2.new(Top2.X, Top2.Y)
+
+                    lines.line2.From = Vector2.new(Top2.X, Top2.Y)
+                    lines.line2.To = Vector2.new(Top3.X, Top3.Y)
+
+                    lines.line3.From = Vector2.new(Top3.X, Top3.Y)
+                    lines.line3.To = Vector2.new(Top4.X, Top4.Y)
+
+                    lines.line4.From = Vector2.new(Top4.X, Top4.Y)
+                    lines.line4.To = Vector2.new(Top1.X, Top1.Y)
+
+                    --//Bottom:
+                    lines.line5.From = Vector2.new(Bottom1.X, Bottom1.Y)
+                    lines.line5.To = Vector2.new(Bottom2.X, Bottom2.Y)
+
+                    lines.line6.From = Vector2.new(Bottom2.X, Bottom2.Y)
+                    lines.line6.To = Vector2.new(Bottom3.X, Bottom3.Y)
+
+                    lines.line7.From = Vector2.new(Bottom3.X, Bottom3.Y)
+                    lines.line7.To = Vector2.new(Bottom4.X, Bottom4.Y)
+
+                    lines.line8.From = Vector2.new(Bottom4.X, Bottom4.Y)
+                    lines.line8.To = Vector2.new(Bottom1.X, Bottom1.Y)
+
+                    --//Sides:
+                    lines.line9.From = Vector2.new(Bottom1.X, Bottom1.Y)
+                    lines.line9.To = Vector2.new(Top1.X, Top1.Y)
+
+                    lines.line10.From = Vector2.new(Bottom2.X, Bottom2.Y)
+                    lines.line10.To = Vector2.new(Top2.X, Top2.Y)
+
+                    lines.line11.From = Vector2.new(Bottom3.X, Bottom3.Y)
+                    lines.line11.To = Vector2.new(Top3.X, Top3.Y)
+
+                    lines.line12.From = Vector2.new(Bottom4.X, Bottom4.Y)
+                    lines.line12.To = Vector2.new(Top4.X, Top4.Y)
+
+                    if Autothickness then
+                        local distance = (player.Character.HumanoidRootPart.Position - part.Position).magnitude
+                        local value = math.clamp(1/distance*100, 0.1, 4) --0.1 is min thickness, 6 is max
+                        for u, x in pairs(lines) do
+                            x.Thickness = value
+                        end
+                    else 
+                        for u, x in pairs(lines) do
+                            x.Thickness = Box_Thickness
+                        end
+                    end
+
+                    for u, x in pairs(lines) do
+                        x.Visible = true
+                    end
+                else 
+                    for u, x in pairs(lines) do
+                        x.Visible = false
+                    end
+                end
+            end)
+        end)
+    end
+    coroutine.wrap(Updater)()
+end
