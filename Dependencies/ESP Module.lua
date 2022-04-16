c01b283699ea6a1004cef8bcaec5f3c0dde3ba6e
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-16 11:01:24 +0800

    Update ESP Module.lua

--- Dependencies/ESP Module.lua
+++ Dependencies/ESP Module.lua
@@ -131,7 +131,8 @@ function module.Create3DESP(objected, text, coloror)
         local connection
         connection = game:GetService("RunService").RenderStepped:Connect(function()
             pcall(function()
-                local partpos, onscreen = camera:WorldToViewportPoint(part.Position)
+                local partpos, onscreen = camera:WorldToScreenPoint(part.Position)
+                --local partpos, onscreen = camera:WorldToViewportPoint(part.Position)
                 local destroyed = not objected:IsDescendantOf(workspace)
                 local Distance = (workspace.Camera.CFrame.Position - part.Position).Magnitude
                 name.Text = "["..round(Distance).."] "..text
