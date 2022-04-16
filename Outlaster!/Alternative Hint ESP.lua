9bcc0f03353d35e1611de8d4ca0be903d4453564
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-16 17:20:08 +0800

    Create Alternative Hint ESP.lua

---
+++ Outlaster!/Alternative Hint ESP.lua
@@ -0,0 +1,40 @@
+synlog = NEON:github('belkworks', 'synlog')
+if game.PlaceId ~= 5765666934 then 
+    synlog:error("You must join an Outlaster game to use this script!")
+    return 
+end
+
+local CoSe = game:GetService("CollectionService")
+local twodeeESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Dependencies/Drawing%20Library%202D%20Box%20ESP.lua"))();
+local ContextActionService = game:GetService("ContextActionService")
+local contextButton = false
+
+
+local function search()
+    if contextButton then
+        contextButton = false
+        return
+    else
+        contextButton = true
+    end
+
+    local foundhints = 0
+
+    for i,v in pairs(workspace:GetDescendants()) do
+        if v:IsA("TouchTransmitter") and v.Parent:IsA("MeshPart") and v.Parent.Parent:IsA("Model") and v.Parent.Parent.Parent:IsA("MeshPart") and v.Parent.Parent.Parent.Parent:IsA("Model") and not CoSe:HasTag(v, "ESP") then
+            twodeeESP.ESP(v.Parent, "Advantage Hint", Color3.fromRGB(0,255,0))
+            CoSe:AddTag(v, "ESP")
+            foundhints = foundhints + 1
+        end
+    end
+
+    if foundhints ~= 0 then
+        synlog:success("Found " .. foundhints .. " hints!")
+    else
+        synlog:warning("No new hints were found!")
+    end
+end
+
+ContextActionService:BindAction("V | Search", search, false, Enum.KeyCode.V)
+synlog:success("Script loaded.")
+synlog:info("Press [ V ] to search for hints!")
