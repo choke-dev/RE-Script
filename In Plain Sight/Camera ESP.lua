e69646aa7d644159e3dd7c86dab6ad183f064991
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-21 09:19:41 +0800

    .

---
+++ In Plain Sight/Camera ESP.lua
@@ -0,0 +1,24 @@
+if not NEON then if not isfile('neon/init.lua')then makefolder('neon')local a='https://raw.githubusercontent.com/%s/%s/master/init.lua'writefile('neon/init.lua',game:HttpGet(a:format('belkworks','neon')))end;pcall(loadfile('neon/init.lua'))end
+synlog = NEON:github('belkworks', 'synlog')
+local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Dependencies/ESP%20Module.lua"))()
+local CollectionService = game:GetService("CollectionService")
+local cams = {"Camera1", "Camera2"}
+local foundcams = 0
+
+
+local function isCamera(object)
+    if table.find(cams, object.Name) then
+        return true
+    end
+end
+
+for i,v in pairs(workspace.Map:GetDescendants()) do
+    if v.Name == "Attachment" or "Attatchment" and isCamera(v.Parent) and not CollectionService:HasTag(v.Parent.Part, "3D_ESP") then
+        ESPModule.Create3DESP(v.Parent.Part, v.Parent.Name, Color3.fromRGB(255, 0, 0))
+        synlog:info("Found "..v.Parent.Name)
+    end
+end
+
+if foundcams == 0 then
+    synlog:warn("No cameras found!")
+end
