80eae5fba9b9919fa13c9abf945d2089e4bb723f
choke <47597850+choke-dev@users.noreply.github.com>
2022-04-23 13:37:51 +0800

    yes

---
+++ Prop Hunt!/Hider ESP.lua
@@ -0,0 +1,14 @@
+-- // Services \\ --
+local Players = game:GetService("Players")
+local CollectionService = game:GetService("CollectionService")
+-- // Modules \\ --
+local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Dependencies/ESP%20Module.lua"))()
+
+-- // Main \\ --
+while task.wait() do
+    for i,v in pairs(workspace:GetDescendants()) do
+        if v.Name == "AnimateScript" and v.Parent:IsA("Model") and v:IsA("LocalScript") and not CollectionService:HasTag(v.Parent.HumanoidRootPart, "3D_ESP") then
+            ESPModule.Create3DESP(v.Parent.HumanoidRootPart, "Hider", Color3.fromRGB(112, 149, 120))
+        end
+    end
+end
