f9559ab8df2026f391d3e11d3236e391428279f6
choke <47597850+choke-dev@users.noreply.github.com>
2022-05-14 15:53:18 +0800

    added some info on what the last 2 parameters do + extra

--- Flood Escape 2/Alert System.lua
+++ Flood Escape 2/Alert System.lua
@@ -2,10 +2,26 @@ local module = {}
 
 local remote = game:GetService("ReplicatedStorage").Remote.Alert
 
-function module.newAlert(text, color, textime, event, autoLocalize)
+function module.newAlert(text:string, color:Color3, textime:NumberRange, event:string, autoLocalize:boolean)
 	for i,v in pairs(getconnections(remote.OnClientEvent)) do
 	    v:Fire(text, color, textime, event, autoLocalize)
 	end
 end
 
 return module
+
+--[[
+
+	what the last 2 parameters do:
+	event is just either "event" or "rainbow"
+	├ rainbow is self explanatory
+	└ event makes a flashing text between the color you chose and red just above the hud
+
+	autoLocalize is a boolean that will automatically localize the text
+	└ true will automatically localize the text
+	└ false will not localize the text (default)
+
+	textime is a number range that will determine how long the text will stay on screen
+
+	( i am bad at explaining things :> )
+]]
