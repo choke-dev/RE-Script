8aeb718cc6d82104cef173779c495218ec6e3a98
choke <47597850+choke-dev@users.noreply.github.com>
2022-05-15 08:43:40 +0800

    made load text rainbow wow

--- Flood Escape 2/Infinite Air.lua
+++ Flood Escape 2/Infinite Air.lua
@@ -85,4 +85,4 @@ end)
 ContextActionService:BindAction("InfiniteAirBind", keybindHandler, false, Enum.KeyCode.Q)
 
 AlertModule.newAlert("Press [ Q ] to toggle.", Color3.new(0.188235, 0.447058, 1), 8)
-AlertModule.newAlert("FE2 Infinite Air Loaded.", Color3.new(0.188235, 1, 0.321568), 8)
+AlertModule.newAlert("FE2 Infinite Air Loaded.", Color3.new(0.188235, 1, 0.321568), 8, "rainbow")
