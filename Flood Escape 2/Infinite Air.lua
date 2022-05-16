5bf79a70f2b0ee02a6bdeb23feac6a2ceb43150d
choke <47597850+choke-dev@users.noreply.github.com>
2022-05-16 14:38:38 +0800

    forgot to change url

--- Flood Escape 2/Infinite Air.lua
+++ Flood Escape 2/Infinite Air.lua
@@ -1,5 +1,5 @@
 -- // Services \\ --
-local AlertModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Flood%20Escape%202/Alert%20System.lua"))()
+local FE2Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Flood%20Escape%202/FE2_Library.lua"))()
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local ContextActionService = game:GetService("ContextActionService")
@@ -26,14 +26,14 @@ local function checkState()
                     v.Material = Enum.Material.ForceField
                 end
             end
-            --AlertModule.newAlert("Infinite Air Enabled.", Color3.new(0.188235, 1, 0.321568), 0.4, "event")
+            --FE2Lib.newAlert("Infinite Air Enabled.", Color3.new(0.188235, 1, 0.321568), 0.4, "event")
         else
             for _,v in pairs(LC:GetDescendants()) do
                 if v:IsA("Part") and not table.find(BlacklistedParts, v.Name) then
                     v.Material = Enum.Material.Plastic
                 end
             end
-            --AlertModule.newAlert("Infinite Air Disabled.", Color3.new(1, 0.301960, 0.301960), 0.4, "event")
+            --FE2Lib.newAlert("Infinite Air Disabled.", Color3.new(1, 0.301960, 0.301960), 0.4, "event")
         end
     end)
 end
@@ -84,5 +84,5 @@ end)
 -- // Keybinding \\ --
 ContextActionService:BindAction("InfiniteAirBind", keybindHandler, false, Enum.KeyCode.Q)
 
-AlertModule.newAlert("Press [ Q ] to toggle.", Color3.new(0.188235, 0.447058, 1), 8)
-AlertModule.newAlert("FE2 Infinite Air Loaded.", Color3.new(0.188235, 1, 0.321568), 8, "rainbow")
+FE2Lib.newAlert("Press [ Q ] to toggle.", Color3.new(0.188235, 0.447058, 1), 8)
+FE2Lib.newAlert("FE2 Infinite Air Loaded.", Color3.new(0.188235, 1, 0.321568), 8, "rainbow")
