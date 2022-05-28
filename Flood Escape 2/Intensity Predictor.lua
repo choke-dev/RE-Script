fe8fc3a1863ad2992380a81c3060d9a67412edc5
choke <47597850+choke-dev@users.noreply.github.com>
2022-05-28 14:30:37 +0800

    yeah

--- Flood Escape 2/Intensity Predictor.lua
+++ Flood Escape 2/Intensity Predictor.lua
@@ -1,7 +1,8 @@
 local FE2Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/choke-dev/RE-Script/main/Flood%20Escape%202/FE2_Library.lua"))()
 local ContextActionService = game:GetService("ContextActionService")
 local function round(number, decimalPlaces)
-	return math.round(number * 10^decimalPlaces) * 10^-decimalPlaces
+    local decplaces = decimalPlaces or 0
+	return math.round(number * 10^decplaces) * 10^-decplaces
 end
 local diffBalance = {
     ["Easy"] = 0.4,
@@ -28,19 +29,23 @@ local finalCalculation
 local keybindHandler = function(name, inputState)
     if inputState ~= Enum.UserInputState.Begin then return end
 
-    playersAlive = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Players.Text:split("/")[1]
-    playersJoined = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Players.Text:split("/")[2]:split(" ")[1]
-    intensityValue = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Difficulty.Difficulty.Text:split(":")[1]
-    boostCount = game:GetService("Players").LocalPlayer.PlayerGui.MenuGui.Shop.Window.Content.Categories.BoostIntensity.Desc.Text:split(" ")[2]
+    playersAlive     = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Players.Text:split("/")[1]
+    playersJoined    = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Players.Text:split("/")[2]:split(" ")[1]
+    intensityValue   = game:GetService("Workspace").Lobby.GameInfo.SurfaceGui.Frame.Difficulty.Difficulty.Text:split(":")[1]
+    boostCount       = game:GetService("Players").LocalPlayer.PlayerGui.MenuGui.Shop.Window.Content.Categories.BoostIntensity.Desc.Text:split(" ")[2]
     playerPercentage = playersAlive / playersJoined
     finalCalculation = round(playerPercentage, 2)  + boostCounter[boostCount] - diffBalance[intensityValue]
-    --FE2Lib.newAlert("Calculation Formula: "..round(playerPercentage, 2).." + "..boostCounter[boostCount].." - "..diffBalance[intensityValue])
-    --FE2Lib.newAlert("Final Calculation: "..round(finalCalculation, 2))
-    if string.find(round(finalCalculation, 2), "-") then
-        FE2Lib.newAlert("Predicted Intensity Change: "..round(finalCalculation, 2), Color3.new(1,0,0))
+
+
+    if round(finalCalculation, 2) == 0 then
+        FE2Lib.newAlert("Predicted Intensity Change: "..round(finalCalculation, 2), Color3.new(0.513725, 0.513725, 0.513725))
+    elseif round(finalCalculation, 2) < 0 then
+        FE2Lib.newAlert("Predicted Intensity Change: "..round(finalCalculation, 2), Color3.new(1, 0.286274, 0.286274))
     else
-        FE2Lib.newAlert("Predicted Intensity Change: +"..round(finalCalculation, 2), Color3.new(0,1,0))
+        FE2Lib.newAlert("Predicted Intensity Change: +"..round(finalCalculation, 2), Color3.new(0, 1, 0.533333))
     end
 end
+
+
 
 ContextActionService:BindAction("Calculate", keybindHandler, false, Enum.KeyCode.R)
