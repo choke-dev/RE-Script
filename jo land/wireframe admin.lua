b329a24ec25bac2df915ec8063096519614dc2a7
choke <47597850+choke-dev@users.noreply.github.com>
2022-06-05 13:46:54 +0800

    finally fixed the thing so i can pass args on it now

--- jo land/wireframe admin.lua
+++ jo land/wireframe admin.lua
@@ -2,20 +2,19 @@ local Players = game:GetService("Players")
 local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 local colorRed = Color3.new(1,0.3294117647058824,0.3294117647058824)
 local colorGreen = Color3.new(0.3607843137254902,1,0.596078431372549)
+local commandCount = 0
 local Prefix = "!"
 
-function modifyPlayerHealth(playerName, health)
-    task.spawn(function()
-        repeat
-            game:GetService("ReplicatedStorage").dremote:FireServer(Players[playerName].Character.Humanoid, 2, "flamethrower")
-            task.wait()
-        until Players[playerName].Character.Humanoid.Health == health
-    end)
+-- // Functions \\ --
+function killPlayer(playerName)
+    for i = 1, 50 do
+        game:GetService("ReplicatedStorage").dremote:FireServer(Players[playerName].Character.Humanoid, 2, "flamethrower")
+    end
 end
 
 function newMessage(msg, clr)
     game.StarterGui:SetCore("ChatMakeSystemMessage", { 
-        Text = "[ WIREFRAME ADMIN ] "..msg, 
+        Text = "⌜ WIREFRAME ADMIN ⌟ "..msg, 
         Color = clr, 
         Font = Enum.Font.SourceSansBold, 
         FontSize = Enum.FontSize.Size24,
@@ -27,6 +26,7 @@ function findPlayer(string)
     local lowercase = string:lower()
     local result
     for _, plr in next, Players:GetPlayers() do
+        print(plr.Name:lower().." attempting to match with "..lowercase)
         if plr.Name:sub(1,#string):lower() == lowercase then
             result = plr
             return plr
@@ -43,58 +43,40 @@ function checkPrefix(message)
     end
 end
 
+-- // Commands \\ --
 Commands = {}
 
--- // Commands \\ --
 Commands.kill = function(...)
     local args = {...}
-    local Target = findPlayer(args[1])
-    modifyPlayerHealth(Target.Name, 0)
+    local Target = findPlayer(unpack(args[1]))
+    killPlayer(Target.Name)
     newMessage("Killed "..Target.Name..".", colorRed)
 end
-
-Commands.god = function(...)
-    local args = {...}
-    local Target = findPlayer(args[1])
-    modifyPlayerHealth(Target.Name, 100)
-    newMessage(Target.Name.." is now immortal.", colorGreen)
+for i,v in pairs(Commands) do
+    commandCount += 1
 end
-
-Commands.ungod = function(...)
-    local args = {...}
-    local Target = findPlayer(args[1])
-    modifyPlayerHealth(Target.Name, 1)
-    newMessage(Target.Name.." is no longer immortal.", colorRed)
-end
-
-Commands.heal = function(...)
-    local args = {...}
-    local Target = findPlayer(args[1])
-    local HealthToAdd = 100 - tonumber(args[2])
-    modifyPlayerHealth(Target.Name, -HealthToAdd)
-    newMessage("Gave "..HealthToAdd.." health to "..Target.Name..".", colorGreen)
-end
-
-Commands.hurt = function(...)
-    local args = {...}
-    local Target = findPlayer(args[1])
-    local HealthToTake = 100 - tonumber(args[2])
-    modifyPlayerHealth(Target.Name, HealthToTake)
-    newMessage("Took "..HealthToTake.." health from "..Target.Name..".", colorRed)
-end
-newMessage("Loaded "..#Commands.." commands.", colorGreen)
+newMessage("Loaded "..commandCount.." command(s).", colorGreen)
 
 -- // Command Handler \\ --
 Players.LocalPlayer.Chatted:Connect(function(message)
     if not checkPrefix(message) then return end
 
     local raw_args = string.split(message, " ")
-    local args = table.concat(raw_args, ' ', 2)
-    local RequestedCommand = args[1]:sub(2)
+    local args = {}
+    local RequestedCommand = raw_args[1]:sub(2)
+	
+    for i = 2, #raw_args do
+        table.insert(args, raw_args[i])
+        print(raw_args[i])
+    end
 
     if Commands[RequestedCommand] then
+        print("Running Command: "..RequestedCommand)
+        print("Arguments: "..table.concat(args, ", "))
         Commands[RequestedCommand](args)
+        table.clear(args)
     else
+        table.clear(args)
         return newMessage("\""..RequestedCommand.."\" is not a valid command.", colorRed)
     end
 end)
